c5dcfaa3ab4bc1005dc0c3772bc2a904e50fa750
BabyShaliniChandran <baby.chandran@coforge.com>
2025-12-11 17:22:22 +0530

    add the to do list of week 9

---
+++ ToDoApp/ToDoApp/Program.cs
@@ -0,0 +1,73 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> task = new List<string>();
+        bool run=true;
+
+        while (run)
+        {
+            Console.WriteLine("---------To Do List-----------");
+            Console.WriteLine("1. Add Task");
+            Console.WriteLine("2. View Task");
+            Console.WriteLine("3. Remove Task");
+            Console.WriteLine("4. Exit");
+
+            string choice= Console.ReadLine();
+
+            static void viewTAsk()
+            {   
+                int i=1;
+                if (task.Count != 0)
+                    {
+                        task.ForEach(task => 
+                        {Console.WriteLine($"{i}task");
+                        i++;
+                        }
+                        );
+                        
+                    }
+                    else
+                    {
+                        Console.WriteLine("No task is added!");
+                    }
+            }
+
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("Enter the task");
+                    string input=Console.ReadLine();
+                    task.Add(input);
+                    Console.WriteLine("Task Added!");
+                    break;
+                
+                case "2":
+                    viewTAsk();
+                    break;
+                
+                case "3":
+                    viewTAsk();
+                    Console.WriteLine("Choose the task number to delete the task");
+                    taskno = Console.ReadLine();
+                    task.RemoveAt(taskno);
+                    break;
+
+                case "4":
+                    run=false;
+                    break;
+
+            }
+        }
+
+
+
+
+    }
+
+}
---
+++ week9/Program.cs
@@ -0,0 +1,94 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> task = new List<string>();
+        bool run=true;
+
+        void viewTAsk()
+            {   
+                int i=1;
+                if (task.Count != 0)
+                    {
+                        task.ForEach(task => 
+                        {Console.WriteLine($"{i}.{task}");
+                        i++;
+                        }
+                        );
+                        
+                    }
+                    else
+                    {
+                        Console.WriteLine("No task is added!");
+                    }
+            }
+
+        while (run)
+        {
+            Console.WriteLine("---------To Do List-----------");
+            Console.WriteLine("1. Add Task");
+            Console.WriteLine("2. View Task");
+            Console.WriteLine("3. Remove Task");
+            Console.WriteLine("4. Exit");
+
+            string choice= Console.ReadLine();
+
+            
+
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("Enter the task");
+                    string input=Console.ReadLine();
+                    task.Add(input);
+                    Console.WriteLine("Task Added!");
+                    break;
+                
+                case "2":
+                    viewTAsk();
+                    break;
+                
+                case "3":
+                    if (task.Count!=0){
+                        viewTAsk();
+                        Console.WriteLine("Choose the task number to delete the task");
+                        int  taskno = Convert.ToInt32(Console.ReadLine());
+                        if (taskno > 0 && taskno <= task.Count)
+                        {
+                            task.RemoveAt(taskno - 1); 
+                            Console.WriteLine("Task Removed!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid task number!");
+                        }
+    
+                    }
+                    else
+                    {
+                        Console.WriteLine("no task to remove!");
+                    }
+                    break;
+
+                case "4":
+                    run=false;
+                    break;
+
+                default:
+                    Console.WriteLine("enter the valid number between 1-4!..");
+                    break;
+
+            }
+        }
+
+
+
+
+    }
+
+}
